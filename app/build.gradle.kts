254f0691d36d64d2b6488dc634d07754dc5de535
alllexey <makarovalesha0212@gmail.com>
2025-10-02 18:25:33 +0300

    v1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.4-demo"
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
