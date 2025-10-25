97c578abd103a801cfad2c5c8828d23d20318908
alllexey <alllexey.dev@gmail.com>
2025-10-25 19:16:23 +0300

    Update widgets on boot

--- app/src/main/java/dev/alllexey/itmowidgets/services/BootReceiver.kt
+++ app/src/main/java/dev/alllexey/itmowidgets/services/BootReceiver.kt
@@ -3,14 +3,14 @@ package dev.alllexey.itmowidgets.services
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import dev.alllexey.itmowidgets.ui.widgets.WidgetUtils
 import dev.alllexey.itmowidgets.workers.LessonWidgetUpdateWorker
 import dev.alllexey.itmowidgets.workers.QrWidgetUpdateWorker
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
         if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
-            QrWidgetUpdateWorker.Companion.enqueueImmediateUpdate(context)
-            LessonWidgetUpdateWorker.Companion.enqueueImmediateUpdate(context)
+            WidgetUtils.updateAllWidgets(context)
         }
     }
 }
