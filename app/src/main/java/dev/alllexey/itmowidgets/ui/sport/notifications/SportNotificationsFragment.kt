fe482ad8b2bd6e76c74faf30a28226ac66995257
alllexey <alllexey.dev@gmail.com>
2025-11-18 18:58:01 +0300

    remove notifications fragment

--- app/src/main/java/dev/alllexey/itmowidgets/ui/sport/notifications/SportNotificationsFragment.kt
+++
@@ -1,8 +0,0 @@
-package dev.alllexey.itmowidgets.ui.sport.notifications
-
-import androidx.fragment.app.Fragment
-import dev.alllexey.itmowidgets.R
-
-class SportNotificationsFragment : Fragment(R.layout.fragment_sport_notifications) {
-
-}
